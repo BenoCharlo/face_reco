8a974e6344f4dfa8677cc54bc2489ec5748a7503
KWASSI <kwassi-beno-charles.dokodjo@capgemini.com>
2019-10-08 10:02:45 +0200

    install.sh file changed

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+### Installing Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 cd Desktop
 
 curl -LOk https://github.com/BenoCharlo/face_reco/archive/master.zip
@@ -13,3 +16,5 @@ brew install gtk+3 boost
 brew install dlib
 brew install pkg-config
 pkg-config --libs --cflags dlib-1
+
+pip install -r requirements.txt
