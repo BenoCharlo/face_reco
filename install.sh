555ab3ecb2e3ee66d6ca9271b99832be79b3946e
KWASSI <kwassi-beno-charles.dokodjo@capgemini.com>
2019-10-08 11:43:54 +0200

    change packages version in requirements.txt

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@
 
 brew install python3
 
+brew unlink python
+brew install --ignore-dependencies https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+
+brew switch python 3.6.1
+
 python3 --version
 
 cd Desktop
