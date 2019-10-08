4e50d1304f499559547a638ea8518a60feefd977
KWASSI <kwassi-beno-charles.dokodjo@capgemini.com>
2019-10-08 09:46:32 +0200

    change install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+cd Desktop
+
+curl -LOk https://github.com/BenoCharlo/face_reco/archive/master.zip
+
+unzip master.zip
+
+cd face_reco-master
+
 brew cask install xquartz
 brew install gtk+3 boost
 brew install dlib
