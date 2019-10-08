0cd4569e4d4651ed29766363bdecb88f3c059edb
KWASSI <kwassi-beno-charles.dokodjo@capgemini.com>
2019-10-08 10:17:34 +0200

    install.sh file changed

--- install.sh
+++ install.sh
@@ -5,6 +5,15 @@
 
 cd Desktop
 
+mkdir x_hec_reco
+cd x_hec_reco
+
+pip install virtualenv
+
+virtualenv reco_env
+
+source reco_env/bin/activate
+
 curl -LOk https://github.com/BenoCharlo/face_reco/archive/master.zip
 
 unzip master.zip
