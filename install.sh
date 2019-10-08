fe3ccf392d52302186ef8bf8093e84a3eff8eab2
KWASSI <kwassi-beno-charles.dokodjo@capgemini.com>
2019-10-08 10:38:16 +0200

    change open-cv version in requirements.txt

--- install.sh
+++ install.sh
@@ -3,12 +3,16 @@
 ### Installing Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install python3
+
+python3 --version
+
 cd Desktop
 
 mkdir x_hec_reco
 cd x_hec_reco
 
-pip install virtualenv
+pip3 install virtualenv
 
 virtualenv reco_env
 
@@ -26,4 +30,4 @@ brew install dlib
 brew install pkg-config
 pkg-config --libs --cflags dlib-1
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
